08832ecdf3c9f9e4b71151a93ac7e4a58d0cd5c9
Yahiya Muhammed <yahiyamuhammed55@gmail.com>
2026-01-06 17:04:14 +0530

    feat: (backend) add the cors to allow frontent

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,24 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server is alive");
 });
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
