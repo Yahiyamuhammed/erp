b3553f76db3d33d72f3f34484d298bcb7cb7b772
Yahiya Muhammed <yahiyamuhammed55@gmail.com>
2026-01-06 22:37:57 +0530

    fix: (backend) fix the password sharing to frontent and check for email that already has

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,11 @@ import User from "../models/User.js";
 export const createUser = async (req, res) => {
   const { name, email, password, roleId } = req.body;
 
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await User.create({
@@ -13,7 +18,12 @@ export const createUser = async (req, res) => {
     roleId,
   });
 
-  res.status(201).json(user);
+  res.status(201).json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    roleId: user.roleId,
+  });
 };
 export const getUsers = async (req, res) => {
   const users = await User.find({ isActive: true })
