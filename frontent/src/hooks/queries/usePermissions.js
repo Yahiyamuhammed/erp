1ba96798cc067788f6a7ea3986535a9a23ffc7d3
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-01-08 18:24:36 +0530

    fix (frontent): add imports for usequery and api in usepermissions

--- frontent/src/hooks/queries/usePermissions.js
+++ frontent/src/hooks/queries/usePermissions.js
@@ -1,8 +1,13 @@
+import api from "@/api/axios";
+import { useQuery } from "@tanstack/react-query";
+
 export const usePermissions = () => {
+  console.log('hit usepermission')
   return useQuery({
     queryKey: ["permissions"],
     queryFn: async () => {
       const { data } = await api.get("/permissions");
+      console.log(data)
       return data.permissions;
     },
   });
