06ece69c493468e8b46de53abde50c2b39e8365c
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-01-12 13:42:01 +0530

    fix (backend): corrected imports at company routes

--- backend/src/routes/company.routes.js
+++ backend/src/routes/company.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
-import authMiddleware from "../middlewares/authMiddleware.js";
-import checkPermission from "../middlewares/checkPermission.js";
-import validate from "../middlewares/validate.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 import {
   createCompany,
   getCompanies,
   updateCompany,
 } from "../controllers/company.controller.js";
+import checkPermission from "../middleware/permission.middleware.js";
+import { validate } from "../middleware/validate.js";
 import {
   createCompanySchema,
   updateCompanySchema,
-} from "../validations/company.schema.js";
+} from "../validators/company.schema.js";
 
 const router = express.Router();
 
@@ -22,12 +22,7 @@ router.post(
   createCompany
 );
 
-router.get(
-  "/",
-  authMiddleware,
-  checkPermission("VIEW_COMPANY"),
-  getCompanies
-);
+router.get("/", authMiddleware, checkPermission("VIEW_COMPANY"), getCompanies);
 
 router.put(
   "/:companyId",
