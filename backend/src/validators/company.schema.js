b4ff12e3f578d3eb4cace9cbc39603dfdcee13a2
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-01-12 14:49:41 +0530

    fix (backend): fixed the company schema to update the company

--- backend/src/validators/company.schema.js
+++ backend/src/validators/company.schema.js
@@ -12,28 +12,36 @@ export const createCompanySchema = yup.object({
     .string()
     .trim()
     .uppercase()
+    .matches(
+      /^[A-Z0-9]+$/,
+      "Company code can contain only uppercase letters and numbers"
+    )
     .min(3, "Company code must be at least 3 characters")
     .max(20, "Company code must be less than 20 characters")
     .required("Company code is required"),
 
   gstNo: yup.string().trim().nullable(),
 });
-
 export const updateCompanySchema = yup.object({
   name: yup
     .string()
     .trim()
     .min(3, "Company name must be at least 3 characters")
-    .max(100, "Company name must be less than 100 characters")
-    .required("Company name is required"),
+    .max(100, "Company name must be less than 100 characters"),
+
   code: yup
     .string()
     .trim()
     .uppercase()
+    .matches(
+      /^[A-Z0-9]+$/,
+      "Company code can contain only uppercase letters and numbers"
+    )
     .min(3, "Company code must be at least 3 characters")
     .max(20, "Company code must be less than 20 characters")
-    .required("Company code is required"),
+    .optional(),
 
   gstNo: yup.string().trim().nullable(),
+
   isActive: yup.boolean(),
 });
