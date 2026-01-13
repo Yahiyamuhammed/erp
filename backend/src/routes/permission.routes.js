17b4cd709bc83613910a71ef3bf207d239d92cf4
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-01-13 21:22:57 +0530

    feat (backend): add the company context to the company route

--- backend/src/routes/permission.routes.js
+++ backend/src/routes/permission.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 import checkPermission from "../middleware/permission.middleware.js";
 import { getPermissions } from "../controllers/permission.controller.js";
+import { companyContext } from "../middleware/company.middleware.js";
 
 const router = express.Router();
 
 router.get(
   "/",
   authMiddleware,
+  companyContext,
   checkPermission("UPDATE_ROLE"),
   getPermissions
 );
