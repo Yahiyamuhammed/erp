addfdcf3d36857361850bf0abec83a6a127c182d
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-01-14 11:54:11 +0530

    feat (frontent): made the user management to pass the company id to backend when creating and updating a user

--- frontent/src/hooks/mutations/useUpdateUser.js
+++ frontent/src/hooks/mutations/useUpdateUser.js
@@ -1,12 +1,14 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../../api/axios";
 
-export const useUpdateUser = () => {
+export const useUpdateUser = (companyId) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async ({ userId, payload }) => {
-      const { data } = await api.put(`/users/${userId}`, payload);
+      const { data } = await api.put(`/users/${userId}`, payload,{
+        params: companyId ? { companyId } : {},
+      });
       return data;
     },
     onSuccess: () => {
