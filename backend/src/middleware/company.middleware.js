9f7cff4f58f7bfe7ab79b2c17ddc67f1273cdeab
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-01-14 11:24:54 +0530

    feat (backend): made the company middleware to add company when it is super admin and pass the company id as middleware

--- backend/src/middleware/company.middleware.js
+++ backend/src/middleware/company.middleware.js
@@ -1,8 +1,19 @@
 export const companyContext = (req, res, next) => {
+  // Super admin must explicitly choose a company
   if (req.user.isSuperAdmin) {
+    const companyId = req.query.companyId;
+
+    if (!companyId) {
+      return res.status(400).json({
+        message: "Company context is required",
+      });
+    }
+
+    req.companyId = companyId;
     return next();
   }
 
+  // Normal users
   if (!req.user.companyId) {
     return res.status(403).json({
       message: "User is not assigned to a company",
